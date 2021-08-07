c4171d847e96be5f49387f35c11a3b2934bb41c0
Justin Silver <jsilver@doublesharp.com>
2021-08-07 14:13:27 -0700

    wordpress 5.8, don't install plugins, formatting

--- rootfs/usr/local/bin/install-plugins.sh
+++ rootfs/usr/local/bin/install-plugins.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# ARG ACF_EXTENDED_SRC=https://downloads.wordpress.org/plugin/acf-extended.0.8.7.4.zip
+# ARG ADVANCED_CUSTOM_FIELDS_SRC=https://downloads.wordpress.org/plugin/advanced-custom-fields.5.9.3.zip
+# ARG AMAZON_S3_AND_CLOUDFRONT_SRC=https://downloads.wordpress.org/plugin/amazon-s3-and-cloudfront.2.5.1.zip
+# ARG CUSTOM_POST_TYPE_UI_SRC=https://downloads.wordpress.org/plugin/custom-post-type-ui.1.8.1.zip
+# ARG REDIS_CACHE_SRC=https://downloads.wordpress.org/plugin/redis-cache.2.0.15.zip
+# ARG WP_GRAPHQL_SRC=https://github.com/wp-graphql/wp-graphql/archive/v1.0.zip
+# ARG WP_GRAPHIQL_SRC=https://github.com/wp-graphql/wp-graphiql/archive/v1.0.1.zip
+# ARG WP_GRAPHQL_ACF_SRC=https://github.com/wp-graphql/wp-graphql-acf/archive/v0.4.0.zip
+# ARG WP_GRAPHQL_INSIGHTS_SRC=https://github.com/wp-graphql/wp-graphql-insights/archive/master.zip
+# ARG WP_GRAPHQL_CPT_UI_SRC=https://github.com/wp-graphql/wp-graphql-custom-post-type-ui/archive/v1.1.zip
+# ARG WP_GRAPHQL_JWT_AUTHENTICATION_SRC=https://github.com/wp-graphql/wp-graphql-jwt-authentication/archive/v0.4.1.zip
+# ARG POLYLANG_SRC=https://downloads.wordpress.org/plugin/polylang.2.9.zip
+# ARG POLYLANG_SLUG_SRC=https://github.com/grappler/polylang-slug/archive/master.zip
+# ARG WP_GRAPHQL_POLYLANG_SRC=https://github.com/valu-digital/wp-graphql-polylang/archive/v0.5.0.zip
+
 # exit on errors
 set -xe
 
